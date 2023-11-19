5cdb6427571b4aec805f99019aa9ddec860f9813
“bradeu” <bradeu01@gmail.com>
2023-11-18 21:52:30 -0800

    Finish About Template

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,33 +1,33 @@
 import React from "react";
+import githubLogo from "../public/github-logo.png"
+import linkedinLogo from "../public/linkedin-logo.png"
+import instagramLogo from "../public/instagram-logo.png"
 
 export default function Footer(){
     return (
         <>
-        <footer class="bg-gray-800 text-gray-300">
-            <div class="container mx-auto py-8 px-4">
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div>
-        <h3 class="text-xl font-bold mb-4">Company Name</h3>
-        <p>About Us</p>
-        <p>Contact Us</p>
-      </div>
-      <div>
-        <h3 class="text-xl font-bold mb-4">Products</h3>
-        <p>Product 1</p>
-        <p>Product 2</p>
-      </div>
-      <div>
-        <h3 class="text-xl font-bold mb-4">Follow Us</h3>
-        <p>Social Media Icons/Links</p>
-      </div>
-    </div>
-    <hr class="my-6 border-gray-600"/>
-    <div class="text-center">
-      <p>&copy; 2023 Your Company. All Rights Reserved.</p>
-      <p>Terms of Service | Privacy Policy</p>
-    </div>
-  </div>
-</footer>
+        <footer className="rounded-lg shadow m-4">
+            <div className="footer-container">
+                <p>© 2023 Bradley Eugene Sakran</p>
+                <ul className="links">
+                    <li>
+                        <a href="https://github.com/bradeu">
+                            <img src={githubLogo} />
+                            </a>
+                    </li>
+                    <li>
+                        <a href="https://linkedin.com/in/besakran" >
+                            <img src={linkedinLogo} />
+                            </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/bradley.eugene/" >
+                            <img src={instagramLogo} />
+                            </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
         </>
     )
 }
