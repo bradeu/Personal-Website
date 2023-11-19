3aeacb95ba0fd476019e6df1060928266d8b1d73
“bradeu” <bradeu01@gmail.com>
2023-11-19 15:21:35 -0800

    Add Contact Body and Resume Body

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
-import Body from "./Body";
+import AboutBody from "./AboutBody";
 import Footer from "./Footer";
 
 export default function About() {
   return (
     <>
     <NavigationBar />
-    <Body />
+    <AboutBody />
     <Footer />
     </>
   )
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
+import ContactBody from "./ContactBody";
+import Footer from "./Footer";
 
 export default function Contact() {
   return (
+    <>
     <NavigationBar />
+    <ContactBody />
+    <Footer />
+    </>
   )
 }
